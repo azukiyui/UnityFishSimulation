e611e9cd01508c90391d95ae725616bdceb14cfb
Azuki Yui <azuki.yui.2020@gmail.com>
2020-07-14 11:08:28 +0900

    add fish csript

---
+++ Assets/Scripts/StructureModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityFishSimulation
+{
+    public class StructureModel : MonoBehaviour
+    {
+        public struct MassNode
+        {
+
+            public float mass;
+        }
+        [SerializeField] protected List<MassNode> massNodes = new List<MassNode>();
+
+    }
+}
