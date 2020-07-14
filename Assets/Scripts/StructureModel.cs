c0be2c16358ee8c9e3c9d73f9cf9a19958a1916a
Azuki Yui <azuki.yui.2020@gmail.com>
2020-07-14 11:21:46 +0900

    add math lib

--- Assets/Scripts/StructureModel.cs
+++ Assets/Scripts/StructureModel.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 namespace UnityFishSimulation
 {
     public class StructureModel : MonoBehaviour
     {
+        [System.Serializable]
         public struct MassNode
         {
-
+            public float3 pos;
             public float mass;
         }
         [SerializeField] protected List<MassNode> massNodes = new List<MassNode>();
